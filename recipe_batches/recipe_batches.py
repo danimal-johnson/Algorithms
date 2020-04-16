b482d2f8e72a7570d972810b2654c5d432ccf71d
Dan Johnson <danimal.johnson@gmail.com>
2020-04-16 05:40:49 -0700

    Implements recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,34 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+def recipe_batches(recipe, pantry):
+  """Return an int
 
+  How many batches of the recipe can
+  be made with ingredients in the pantry?
+  """
+
+  max_batches = -1
+
+  # The minimum ingredients are listed in the recipe dictionary, so we need to walk through it once.
+  for ingredient in recipe:
+    if ingredient in pantry:
+      # print(ingredient, '->', recipe[ingredient], pantry[ingredient])
+
+      if max_batches == -1: # First pass. Start calculating max dishes from scratch.
+        max_batches = pantry[ingredient]//recipe[ingredient]
+      else:
+        max_batches =  min(max_batches, (pantry[ingredient]//recipe[ingredient]))
+
+    else: # Insufficient ingredients. Exit.
+      # print(ingredient, '->', recipe[ingredient], "None")
+      return 0
+
+  return max_batches
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
+  # Test example
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+    batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
